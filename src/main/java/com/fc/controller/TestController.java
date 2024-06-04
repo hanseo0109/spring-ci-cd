76777a5260df4778b85044da03a825f7350e1f37
hanseo0109 <hanseo0109@gmail.com>
2024-06-05 01:48:24 +0900

    add

--- src/main/java/com/fc/controller/TestController.java
+++ src/main/java/com/fc/controller/TestController.java
@@ -3,7 +3,11 @@ package com.fc.controller;
 import com.fc.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class TestController {
     private TestService testService;
 
